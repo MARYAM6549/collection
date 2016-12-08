b5e4e9cded0a2b11845fe026689668962a4be9c4
maryam^c <maryam.abbasi.6549@gmail.com>
2016-12-08 08:47:24 +0330

    Hollow inverted mirrored right triangle

---
+++ 14.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  for (int i = 1; i < 6; i++) {
+  for (int j = 1; j < 6; j++)
+      {
+      if(i==1||j==5)  cout<<"* ";
+      else if(j==i)  cout<<"* ";
+      else cout<<"  ";
+      }
+    cout<<"\n";
+  }
+  return 0;
+}
