2bde7a73924adfa9638f076956c78d3fdcd12c20
maryam^c <maryam.abbasi.6549@gmail.com>
2016-12-08 08:21:21 +0330

    Square

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  for (int i = 1; i < 6; i++)
+      {
+          for (int j = 1; j < 6; j++) {
+      cout<<"* ";
+      }
+  cout<<"\n";
+  }
+  return 0;
+}
