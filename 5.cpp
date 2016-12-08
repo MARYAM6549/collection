2a36a3435bae0de87c66b66d79a65daf101b0a87
maryam^c <maryam.abbasi.6549@gmail.com>
2016-12-08 08:34:35 +0330

    Mirrored Rhombus

---
+++ 5.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  for (int i = 0; i < 5; i++) {
+  for (int j = 0; j < i; j++) {
+    cout<<" ";
+  }
+  for (int k = 0; k < 5; k++) {
+  cout<<"* ";
+  }
+    cout<<"\n";
+  }
+    return 0;
+  }
