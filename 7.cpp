f9c22abf6d7d0ea918b3043c3b825e61ee5dfc40
maryam^c <maryam.abbasi.6549@gmail.com>
2016-12-08 08:37:57 +0330

    Right triangle

---
+++ 7.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  for (int i = 0; i < 6; i++) {
+  for (int j = 0; j < i; j++) {
+cout<<"* ";
+  }
+cout<<"\n";
+  }
+    return 0;
+  }
